9a3a0e6874acbc88d0f8fda12dbe178660710d9a
pontus393 <pontus.loefgren@gmail.com>
2023-05-17 18:17:12 +0200

    Change container width and assign unique id:s to each square

--- script.js
+++ script.js
@@ -2,16 +2,15 @@
 let numberOfSquares = 16;
 
 // Create div square
-// your JavaScript file
 const container = document.querySelector('#container');
 
 // create # of squares in a loop 
 for (let i = 0; i < numberOfSquares; i++) {
     const square = document.createElement('div');
+    square.setAttribute('id', i+1);
     square.classList.add('square');
     container.appendChild(square);
   }
-
 
 
 // Assign CSS style - calculate height and width based on number of squares
